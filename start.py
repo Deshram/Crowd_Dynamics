4513107a566c1959c60cfa530cfc33f32cd6210f
Ram Deshmukh <ram27599@gmail.com>
2020-03-14 13:03:28 +0530

    ui changes

--- start.py
+++ start.py
@@ -6,21 +6,22 @@ from PIL import Image, ImageDraw, ImageTk, ImageFont
 r = tk.Tk() 
 r.title('Crowd Estimation')
 r.geometry('400x600') 
-
-im = Image.open('bg.jpg')
+r.configure(background='white')
+im = Image.open('bg2.PNG')
 im = im.resize((400,600))
 tkimage = ImageTk.PhotoImage(im)
 myvar=tk.Label(r,image = tkimage)
-myvar.place(x=0, y=0, relwidth=1, relheight=1)
+myvar.pack(side = "top",fill = "both",expand = "yes")
+myvar.place(x=0, y=0)
 
 def run():
 	os.system('python crmon1.py')
 
-button1 = tk.Button(r,text='Crowd Monitoring', font = ('Bold',10),width = '25',command = run)
-button1.place(x=100, y=200)
+button1 = tk.Button(r,highlightbackground='black',bg = 'grey',bd = '5',text='Crowd Monitoring', font = ("Allegro", 12, "bold"),width = '25',command = run)
+button1.place(x=75, y=200)
 
-button2 = tk.Button(r,text='Crowd Prediction', font = ('Bold',10),width = '25',command = run)
-button2.place(x=100, y=300)
+button2 = tk.Button(r,text='Crowd Prediction',bg = 'grey',bd = '5', font = ("Allegro", 12, "bold"),width = '25',command = run)
+button2.place(x=75, y=300)
 
 
 r.mainloop()
